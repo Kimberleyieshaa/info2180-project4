01c4ea40ac16e7480900660cb3d0eb2eb19dfbf8
Kimmie01 <kimberleymcdowell@gmail.com>
2014-12-04 18:38:17 -0500

    now logs in and set user session

--- connect.php
+++ connect.php
@@ -1,18 +1,4 @@
 <?php
 	$db_connect = mysql_connect("localhost", "root", "");
 	$db_select = mysql_select_db("cheapo_database");
-
-	$username = $_REQUEST["username"];
-	$password = $_REQUEST["password"];
-
-	$query = mysql_query("SELECT * FROM User");
-
-	while ($row = mysql_fetch_array($query)) {
-		if ($password == $row['password'] {
-			$_SESSION['username'] = $username;
-			echo "hello world!! :) ";
-		}
-
-	}
-
 ?>
--- login.php
+++ login.php
@@ -1,3 +1,16 @@
 <?php
 	
+	require "connect.php";
+
+	$username = $_REQUEST["username"];
+	$password = $_REQUEST["password"];
+	$query = mysql_query("SELECT * FROM User");
+	while ($row = mysql_fetch_array($query)) {
+		if ($username == $row['username'] && $password == $row['password']) {
+			$_SESSION['username'] = $username;
+			echo "hello world!! :) ";
+		} else {
+			echo "incorrect username and password";
+		}
+	}
 ?>
