90662f8ab2bda897b72149681505e12cfb29b3ff
renaldopringle <rapchristoff@gmail.com>
2014-12-12 23:04:22 -0500

    update databse file

--- cheapo_database.sql
+++ cheapo_database.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.11
+-- version 4.2.7.1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Dec 05, 2014 at 12:21 AM
--- Server version: 5.6.21
--- PHP Version: 5.6.3
+-- Generation Time: Dec 13, 2014 at 04:54 AM
+-- Server version: 5.6.20
+-- PHP Version: 5.5.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -32,7 +32,7 @@ CREATE TABLE IF NOT EXISTS `Message` (
   `subject` varchar(255) NOT NULL,
   `user_id` int(11) NOT NULL,
   `recipient_ids` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
 -- --------------------------------------------------------
 
@@ -44,8 +44,8 @@ CREATE TABLE IF NOT EXISTS `Message_read` (
 `id` int(11) NOT NULL,
   `message_id` int(11) NOT NULL,
   `reader_id` int(11) NOT NULL,
-  `date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `dates` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=35 ;
 
 -- --------------------------------------------------------
 
@@ -59,14 +59,16 @@ CREATE TABLE IF NOT EXISTS `User` (
   `lastname` varchar(25) NOT NULL,
   `username` varchar(25) NOT NULL,
   `password` varchar(8) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
 -- Dumping data for table `User`
 --
 
 INSERT INTO `User` (`id`, `firstname`, `lastname`, `username`, `password`) VALUES
-(1, 'Jessica', 'Brown', 'Jess_Brown', 'Pass0001');
+(1, 'Jessica', 'Brown', 'Jess_Brown', 'Pass0001'),
+(2, 'Renaldo', 'Pringle', 'renaldo', 'Rap00001'),
+(3, 'Kinberley', 'McDowell', 'kimmie', 'Kimmie01');
 
 --
 -- Indexes for dumped tables
@@ -98,17 +100,17 @@ ALTER TABLE `User`
 -- AUTO_INCREMENT for table `Message`
 --
 ALTER TABLE `Message`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `Message_read`
 --
 ALTER TABLE `Message_read`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=35;
 --
 -- AUTO_INCREMENT for table `User`
 --
 ALTER TABLE `User`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
